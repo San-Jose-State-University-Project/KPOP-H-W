ea7928ac97c74b24c90186050021b5488ddfba05
bitbyte08 <eight2823@naver.com>
2025-05-12 23:24:32 +0900

    fix :: WiFi 잡히기 전에 넘어가는 문제 해결

--- src/WiFiSetup.cpp
+++ src/WiFiSetup.cpp
@@ -3,7 +3,8 @@
 
 void WiFiSetup::begin(const char* apName) {
     wifiManager.setDebugOutput(false);
-    wifiManager.setConfigPortalTimeout(5);
+    //wifiManager.resetSettings();
+    //wifiManager.setConfigPortalTimeout(5);
     bool res = wifiManager.autoConnect(apName,"password");
     if (res) printLCD("Connect","WiFi");
     else printLCD("Disconnect","WiFi");
