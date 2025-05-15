bdd1237cefcc29e0600a187dca8fba95b98000d6
bitbyte08 <eight2823@naver.com>
2025-05-15 10:37:51 +0900

    feat :: 버튼으로 페이지 전환 구현

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,75 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 #include <Arduino.h>
-// LCD 객체 생성 - 주소 0x27, 16x2
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+#include "WiFiSetup.h"
+#include "I2C.h"
+#include "ApiClient.h"
+#include "PageManager.h"
+#include "WebClient.h"
+
+#define BUTTON1_PIN D3  // GPIO0
+#define BUTTON2_PIN D4  // GPIO2
+#define DEBOUNCE_CYCLES 1600000  // 약 200ms (80MHz 기준)
+
+volatile uint32_t lastButton1Cycle = 0;
+volatile uint32_t lastButton2Cycle = 0;
+
+WiFiSetup wifi;
+PageManager pageManager;
+ApiClient apiClient(&pageManager);
+WebClient webClient(&apiClient);
+
+void IRAM_ATTR handleButton1() {
+    uint32_t now = ESP.getCycleCount();
+    if (now - lastButton1Cycle > DEBOUNCE_CYCLES) {
+        lastButton1Cycle = now;
+        pageManager.nextPage();
+    }
+}
+
+void IRAM_ATTR handleButton2() {
+    uint32_t now = ESP.getCycleCount();
+    if (now - lastButton2Cycle > DEBOUNCE_CYCLES) {
+        lastButton2Cycle = now;
+        pageManager.nextPage();
+    }
+}
 
 void setup() {
-    Wire.begin(D2, D1); // NodeMCU 전용: SDA = D2 (GPIO4), SCL = D1 (GPIO5)
-    lcd.init();         // LCD 초기화
-    lcd.backlight();    // 백라이트 켜기
-
-    lcd.setCursor(0, 0);
-    lcd.print("NodeMCU Hello!");
-    lcd.setCursor(0, 1);
-    lcd.print("LCD 0x27 OK");
+    Serial.begin(115200);
+    initLCD();
+    wifi.begin();
+    apiClient.setDebug(true);
+    apiClient.loadFromEEPROM();
+    webClient.setPageManager(&pageManager);
+    webClient.begin();
+    pinMode(BUTTON1_PIN, INPUT_PULLUP);
+    pinMode(BUTTON2_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(BUTTON1_PIN), handleButton1, FALLING);
+    attachInterrupt(digitalPinToInterrupt(BUTTON2_PIN), handleButton2, FALLING);
 }
-
 void loop() {
-    // loop 비워둬도 OK
+    Serial.println(ESP.getCycleCount());
+    webClient.handleClient();
+    apiClient.httpGet();
 }
+
+
+/*
+*#include "PageManager.h"
+
+PageManager pageManager;
+
+void IRAM_ATTR handleButtonPress() {
+    pageManager.nextPage();
+}
+
+void setup() {
+    pinMode(D3, INPUT_PULLUP); // 버튼 연결된 핀
+    attachInterrupt(digitalPinToInterrupt(D3), handleButtonPress, FALLING);
+
+    pageManager.addPage("Page 1", "WiFi Info");
+    pageManager.addPage("Page 2", "API Title");
+    pageManager.addPage("Page 3", "Other Info");
+
+    pageManager.showCurrentPage();
+}
+ */
